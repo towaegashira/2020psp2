e5f11228c8a0729bfa6d9599ff71e0b675b7a8c5
Egashira Towa <bb35519011@ms.nagasaki-u.ac.jp>
2020-04-23 00:37:56 +0900

    04/22  #1回目

--- k01/k01.c
+++ k01/k01.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val, double i,double ave);
+extern double var_online(double ave,double val,double i,double square_ave);
 
 int main(void)
 {
@@ -12,6 +12,11 @@ int main(void)
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
+    int  i = 0;
+    double ave = 0;
+    double square_ave = 0;
+    double ret;
+    double ans;
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -26,13 +31,13 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
-
-
-
+        i = i + 1.0;
+        ret = ave_online(val,i,ave);
+        ans = var_online(ave,val,i,square_ave);
     }
+
+    printf("%f\n",ret);
+    printf("%f\n",ans);
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
@@ -45,3 +50,14 @@ int main(void)
 
 }
 
+double ave_online(double val,double i,double ave){
+    return ave = ((i-1.0)*ave+val)/i;
+}
+
+double var_online(double ave,double val,double i,double square_ave){
+    ave = ((i-1.0)*ave+val)/i;
+    square_ave = ((i-1.0)*square_ave+pow(val,2.0))/i;
+    return square_ave - pow(ave,2.0);
+
+
+}
